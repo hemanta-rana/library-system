470bf7a9f42e33c666483b5e610737daee4beddc
Hemanta Rana <magarhemanta145@gmail.com>
2025-07-07 12:15:35 +0545

    initail commit

---
+++ src/Book.java
@@ -0,0 +1,25 @@
+public class Book {
+//    attributes -> book number, name, quantity, author
+
+    private  int bookNumber ;
+    private  String name ;
+    private  int quantity ;
+    private  String author;
+
+
+    public Book(int bookNumber, String name, int quantity, String author){
+        this.bookNumber = bookNumber;
+        this.name = name;
+        this.quantity = quantity;
+        this.author = author;
+    }
+
+//    behaviour -> increase quantity, decrease quantity
+    public  void increaseQuantity(){
+
+    }
+
+    public void decreaseQuantity (){
+
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,28 @@
+public class User {
+    private String name ;
+    private String username ;
+    private String contact;
+
+
+    public User(String name, String username, String contact){
+        this.name = name;
+        this.username = username;
+        this.contact = contact;
+
+    }
+
+//    behavior -> check registered, borrow, return
+
+    public void isRegistered(){
+
+    }
+    public void returnBook(){
+
+    }
+    public  void borrowBook(){
+
+    }
+
+
+
+}
